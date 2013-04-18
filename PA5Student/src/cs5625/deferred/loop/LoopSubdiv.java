a2357e680075d2e06584e4b53b92234b3e26a602
bae43 <bae43@cornell.edu>
2013-04-18 18:56:55 -0400

    debugging - changed structure of IDs for vs,fs

--- PA5Student/src/cs5625/deferred/loop/LoopSubdiv.java
+++ PA5Student/src/cs5625/deferred/loop/LoopSubdiv.java
@@ -37,21 +37,27 @@ public class LoopSubdiv {
 	public LoopSubdiv(EdgeDS edgeDS)
 	{
 		// REMOVE
-		Set<Integer> vids = edgeDS.getVertexIDs();
-		for(int id : vids){
-			System.out.println(id+", " + edgeDS.getVertexData(id).mData.getPosition());
-		}
-		System.out.println("=====================");
+//		Set<Integer> vids = edgeDS.getVertexIDs();
+//		for(int id : vids){
+//			System.out.println(id+", " + edgeDS.getVertexData(id).mData.getPosition());
+//		}
+//		System.out.println("=====================");
+		
+		
 		
 		// Maps for new verts, edges, polys
-		TreeMap<Integer, VertexAttributeData> newVerts = new TreeMap<Integer, VertexAttributeData>();
-		int newVertID = 0;
+		TreeMap<Integer, VertexData> newVerts = new TreeMap<Integer, VertexData>();
+		int newVertID = edgeDS.getVertexIDs().size();
+		for(Integer vertID : edgeDS.getVertexIDs()) {
+			newVerts.put(vertID, edgeDS.getVertexData(vertID));
+		}
 		
 		TreeMap<Integer, EdgeData> newEdges = new TreeMap<Integer, EdgeData>();
-		int newEdgeID = 0;
+		int newEdgeID = edgeDS.getEdgeIDs().size();
 		
 		TreeMap<Integer, PolygonData> newPolys = new TreeMap<Integer, PolygonData>();
-		int newPolyID = 0;
+		int newPolyID = edgeDS.getPolygonIDs().size();
+		
 		
 		//Scan through all edges and create new vertices with correct positions
 		for(Integer edgeID : edgeDS.getEdgeIDs()) {
@@ -120,10 +126,11 @@ public class LoopSubdiv {
 			}
 			
 			// REMOVE
-			System.out.println(newVertID+", "+ pos);
+			//System.out.println(newVertID+", "+ pos);
 			
 			 //Inserting vertex into our TreeMap
-			newVerts.put(newVertID, new VertexAttributeData(pos, tex));
+			VertexData vertdata = new VertexData(new VertexAttributeData(pos,tex));
+			newVerts.put(newVertID, vertdata); //Put new positions of even vertices in our TreeMap
 			
 			//Storing the new vertex in the edge it was created in
 			edge.setVertexIDNew(newVertID++); 
@@ -162,61 +169,95 @@ public class LoopSubdiv {
 				pos.add(temp);
 				tex.add(temp1);
 			}
-			newVerts.put(newVertID, new VertexAttributeData(pos, tex)); //Put new positions of even vertices in our TreeMap
+			VertexData vertdata = new VertexData(new VertexAttributeData(pos,tex));
+			newVerts.put(newVertID, vertdata); //Put new positions of even vertices in our TreeMap
 			vert.setNewVertexID(newVertID++); //Setting the old vertex IDs to the new ones we created
 		}
 		
 
 		//Scan through all the pre-existing edges to create new ones
 		for(Integer edgeID : edgeDS.getEdgeIDs()) {
+			
 			EdgeData edge = edgeDS.getEdgeData(edgeID);
 			int newVertex = edge.getNewVertexID();
+			
 			int vert0 = edge.getVertex0();
 			int vert1 = edge.getVertex1();
+			
 			int edge0n = newEdgeID++;
-			newEdges.put(edge0n, new EdgeData(vert0,newVertex)); //For each edge put an edge between the one old vertex and the new one
 			int edgen1 = newEdgeID++;
+			
+			newEdges.put(edge0n, new EdgeData(vert0,newVertex)); //For each edge put an edge between the one old vertex and the new one			
 			newEdges.put(edgen1, new EdgeData(newVertex,vert1)); // and  an edge between the new one and the other old vertex
 			
 			//Store connectivity (in pairs)
-			edgeDS.getVertexData(vert0).addVertexConnectivity(new EdgeVertexPair(edge0n, newVertex));
-			edgeDS.getVertexData(newVertex).addVertexConnectivity(new EdgeVertexPair(edge0n, vert0));
+			newVerts.get(vert0).addVertexConnectivity(new EdgeVertexPair(edge0n, newVertex));
 			
-			edgeDS.getVertexData(newVertex).addVertexConnectivity(new EdgeVertexPair(edgen1, vert1));
-			edgeDS.getVertexData(vert1).addVertexConnectivity(new EdgeVertexPair(edgen1, newVertex));	
+			
+//			ret.setPolygonData(IntBuffer polys);
+//			ret.setEdgeData(IntBuffer edges);
+//			ret.setVertexData(FloatBuffer vertices);
+			
+			newVerts.get(newVertex).addVertexConnectivity(new EdgeVertexPair(edge0n, vert0));
+			newVerts.get(newVertex).addVertexConnectivity(new EdgeVertexPair(edgen1, vert1));
+			newVerts.get(vert1).addVertexConnectivity(new EdgeVertexPair(edgen1, newVertex));	
 		}
+		
+		System.out.println("CURRENT STATE DATA: \n ===============");
+		for(Integer vertID : newVerts.keySet()) {
+			System.out.println("v"+vertID +": " + newVerts.get(vertID).mData.getPosition());
+		}
+		for(Integer edgeID : newEdges.keySet()) {
+			System.out.println("e"+edgeID +": (" + newEdges.get(edgeID).getVertex0()+", " +newEdges.get(edgeID).getVertex1()+")");
+		}
+		for(Integer faceID : newPolys.keySet()) {
+			System.out.println("f"+faceID +": " + newPolys.get(faceID).getAllEdges());
+		}
+		
 		
 		//Scan through all the pre-existing polygons to create new ones and the remaining edges
 		for(Integer polyID : edgeDS.getPolygonIDs()) {
 			PolygonData poly = edgeDS.getPolygonData(polyID);
 			ArrayList<Integer> allEdges = poly.getAllEdges();
 			
+			System.out.println(polyID+": " + allEdges);
+			System.out.println(allEdges.get(0));
+			
 			//Find the new vertices of the face
+			// XXX RESUME - remove need for edgeDS - causing errors because it contains invalid data.
 			int newvert0 = edgeDS.getEdgeData(allEdges.get(0)).getNewVertexID();
 			int newvert1 = edgeDS.getEdgeData(allEdges.get(1)).getNewVertexID();
 			int newvert2 = edgeDS.getEdgeData(allEdges.get(2)).getNewVertexID();
-			System.out.println(newvert0 +", "+newvert1 +", "+newvert2);
 			
 			//Insert the 3 new edges between the new vertices
 			int edgenv0nv1 = newEdgeID++;
-			newEdges.put(edgenv0nv1, new EdgeData(newvert0,newvert1));
 			int edgenv1nv2 = newEdgeID++;
-			newEdges.put(edgenv1nv2, new EdgeData(newvert1,newvert2));
 			int edgenv2nv0 = newEdgeID++;
+			
+			newEdges.put(edgenv0nv1, new EdgeData(newvert0,newvert1));			
+			newEdges.put(edgenv1nv2, new EdgeData(newvert1,newvert2));			
 			newEdges.put(edgenv2nv0, new EdgeData(newvert2,newvert0));
 			
 			//Get the 3 pre-existing vertices in the triangle
 			int vert0 = edgeDS.getEdgeData(allEdges.get(0)).getVertex0();
 			int vert1 = edgeDS.getEdgeData(allEdges.get(1)).getVertex0();
+			//change to other vertex if needed
+			vert1 = vert1 != vert0 ? vert1 : edgeDS.getEdgeData(allEdges.get(1)).getVertex1();
 			int vert2 = edgeDS.getEdgeData(allEdges.get(2)).getVertex0();
+			vert2 = vert2 != vert0 && vert2 != vert1 ? vert2 : edgeDS.getEdgeData(allEdges.get(1)).getVertex1();
+			
+			System.out.println(poly.getAllVertices());
+			System.out.println("verts: " + vert0 +","+vert1+","+vert2);
+			System.out.println("intermediate verts: " + newvert0 +","+newvert1+","+newvert2);
 			
 			//Get the 6 edges connecting the 3 pre-existing vertices to the new ones
-			int edge0nv0 = edgeDS.getVertexData(vert0).getEdgeIDforEdgeWithThisVertex(newvert0).getData();
-			int edgenv01 = edgeDS.getVertexData(vert1).getEdgeIDforEdgeWithThisVertex(newvert0).getData();
-			int edge1nv1 = edgeDS.getVertexData(vert1).getEdgeIDforEdgeWithThisVertex(newvert1).getData();
-			int edgenv12 = edgeDS.getVertexData(vert2).getEdgeIDforEdgeWithThisVertex(newvert1).getData();
-			int edge2nv2 = edgeDS.getVertexData(vert2).getEdgeIDforEdgeWithThisVertex(newvert2).getData();
-			int edgenv20 = edgeDS.getVertexData(vert0).getEdgeIDforEdgeWithThisVertex(newvert2).getData();
+			int edge0nv0 = newVerts.get(vert0).getEdgeIDforEdgeWithThisVertex(newvert0).getData();
+			System.out.println("CANT FIND EDGE WITH: " + vert1 + " , " + newvert0);
+			int edgenv01 = newVerts.get(vert1).getEdgeIDforEdgeWithThisVertex(newvert0).getData();
+			int edge1nv1 = newVerts.get(vert1).getEdgeIDforEdgeWithThisVertex(newvert1).getData();
+			int edgenv12 = newVerts.get(vert2).getEdgeIDforEdgeWithThisVertex(newvert1).getData();
+			int edge2nv2 = newVerts.get(vert2).getEdgeIDforEdgeWithThisVertex(newvert2).getData();
+			int edgenv20 = newVerts.get(vert0).getEdgeIDforEdgeWithThisVertex(newvert2).getData();
 			
 			//Create the 4 polygons of a triangular face accordingly
 			newPolys.put(newPolyID++, new PolygonData(edgenv0nv1,edgenv1nv2,edgenv2nv0,newvert0,newvert1,newvert2));
